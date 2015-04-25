e36ae4f1a49e148515e26d96c3024a50daca9e1d
Ivan Uvarov <tonk2012@gmail.com>
2015-04-25 03:05:42 -0400

    Basic routing and views using Hapi

---
+++ app.js
@@ -0,0 +1,47 @@
+var Handlebars = require('handlebars');
+var Path = require('path');
+var Hapi = require('hapi');
+
+// Create a server with a host and port
+var server = new Hapi.Server();
+server.connection({
+  host: 'localhost',
+  port: 3000
+});
+
+server.views({
+  engines: {
+    html: Handlebars.create()
+  },
+  path: Path.join(__dirname, 'views'),
+  layoutPath: Path.join(__dirname, 'views/layout'),
+  layout: true,
+  partialsPath: Path.join(__dirname, 'views/partials')
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, reply) {
+    reply("Here's the main page.");
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/users/{username}',
+  handler: function(request, reply) {
+    reply("username: " + encodeURIComponent(request.params.username));
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/views-test',
+  handler: function(request, reply) {
+    reply.view('item', {title: 'Item Title', body: 'Item Body'})
+  }
+});
+
+// Start the server
+server.start();
